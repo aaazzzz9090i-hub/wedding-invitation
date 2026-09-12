e777c2596a33425e73694a191ba3e9892c13f0f1
aaazzzz9090i-hub <aaazzzz9090i@gmail.com>
2026-09-12 04:18:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 const envelope = document.getElementById("envelope");
+const opening = document.getElementById("opening");
+const invitation = document.getElementById("invitation");
 
-let isOpened = false;
+let opened = false;
 
 function openEnvelope() {
-    if (isOpened) return;
 
-    isOpened = true;
+    if (opened) return;
+
+    opened = true;
+
+    // Start envelope opening animation
     envelope.classList.add("open");
+
+    /*
+        Sequence:
+
+        0.00s  → envelope starts opening
+        0.15s  → flap starts opening
+        0.55s  → letter starts coming out
+        2.40s  → transition to invitation
+    */
+
+    setTimeout(() => {
+
+        opening.classList.add("hide");
+        invitation.classList.add("show");
+
+    }, 2500);
 }
 
-envelope.addEventListener("click", openEnvelope);
-envelope.addEventListener("touchend", openEnvelope, { passive: true });
+
+/*
+    Event delegation
+    Works with:
+    - Laptop
+    - Desktop
+    - iPhone
+    - Android
+*/
+
+document.addEventListener("click", function (event) {
+
+    const envelopeClicked =
+        event.target.closest("#envelope");
+
+    if (!envelopeClicked) return;
+
+    openEnvelope();
+});
