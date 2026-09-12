f5f8b247c528cd83304a5e4906ed7247ccecedb5
aaazzzz9090i-hub <aaazzzz9090i@gmail.com>
2026-09-12 04:00:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 const envelope = document.getElementById("envelope");
-const seal = document.getElementById("seal");
 
 let isOpened = false;
 
 function openEnvelope(event) {
-
     if (isOpened) return;
 
     isOpened = true;
 
     if (event) {
         event.preventDefault();
-        event.stopPropagation();
     }
 
     envelope.classList.add("open");
 }
 
-envelope.addEventListener("click", openEnvelope);
-envelope.addEventListener("touchstart", openEnvelope, {
-    passive: false
-});
-
-seal.addEventListener("click", openEnvelope);
-seal.addEventListener("touchstart", openEnvelope, {
+envelope.addEventListener("pointerdown", openEnvelope, {
     passive: false
 });
