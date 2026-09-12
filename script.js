7cbc67509c5d4667e77a156156e05170189280eb
aaazzzz9090i-hub <aaazzzz9090i@gmail.com>
2026-09-12 04:06:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 const envelope = document.getElementById("envelope");
+const opening = document.getElementById("opening");
 
 let isOpened = false;
 
 function openEnvelope(event) {
     if (isOpened) return;
 
+    const target = event.target;
+
+    // نتأكد إن الضغط كان على الظرف نفسه
+    if (!target.closest("#envelope")) return;
+
     isOpened = true;
 
-    if (event) {
+    if (event.cancelable) {
         event.preventDefault();
     }
 
     envelope.classList.add("open");
 }
 
-envelope.addEventListener("pointerdown", openEnvelope, {
+
+// Mouse + modern mobile
+opening.addEventListener("pointerdown", openEnvelope, {
     passive: false
 });
+
+
+// iPhone / Safari fallback
+opening.addEventListener("touchend", openEnvelope, {
+    passive: false
+});
+
+
+// Final fallback
+opening.addEventListener("click", openEnvelope);
